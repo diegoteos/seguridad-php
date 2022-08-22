3ae77e1f869d256d731656c750551811ab88dc21
diegoteos <diegoteos@hotmail.com>
2022-08-22 15:54:57 -0600

    hashear un archivo

--- seguridad/hash.php
+++ seguridad/hash.php
@@ -3,3 +3,5 @@
 echo '<br>' . hash_file('md5', 'pago.php');
 echo '<br>' . hash_file('sha256', 'pago.php');
 echo '<br>' . hash_file('sha512', 'pago.php');
+//echo '<br>' . hash_file('sha512', 'E:\Users\Diego\Desktop\Windows.iso');
+echo '<br> Obtener el valor hash de la imagen iso es detallando su ruta: <br>' . hash_file('sha512', 'E:/Users/Diego/Desktop/Windows.iso');
