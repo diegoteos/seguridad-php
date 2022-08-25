62135704370c32f2c7dbf9b117550101b19ac202
diegoteos <diegoteos@hotmail.com>
2022-08-25 11:44:35 -0600

    actualizacion

--- seguridad/hash.php
+++ seguridad/hash.php
@@ -3,5 +3,6 @@
 echo '<br>' . hash_file('md5', 'pago.php');
 echo '<br>' . hash_file('sha256', 'pago.php');
 echo '<br>' . hash_file('sha512', 'pago.php');
-//echo '<br>' . hash_file('sha512', 'E:\Users\Diego\Desktop\Windows.iso');
-echo '<br> Obtener el valor hash de la imagen iso es detallando su ruta: <br>' . hash_file('sha512', 'E:/Users/Diego/Desktop/Windows.iso');
+echo  '<br><br>';
+//echo '<br>' . hash_file('md5', 'F:\MATERIAL.PROYECTO\EVIDENCIAS\Disk.Partition.Raw.Image');
+echo '<br> Obtener el valor hash de una imagen iso detallando unicamente el tipo de hash, su ruta y nombre: <br>' . hash_file('md5', 'E:\Users\Diego\Desktop\UMG\forense\practica2\DiskPartitionRawImage.dd');
