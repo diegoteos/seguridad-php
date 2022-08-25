6b7d86e8e5003f89a28e7b369cbd60b15c28ec64
diegoteos <diegoteos@hotmail.com>
2022-08-25 11:51:05 -0600

    Update hash.php

--- seguridad/hash.php
+++ seguridad/hash.php
@@ -5,4 +5,4 @@ echo '<br>' . hash_file('sha256', 'pago.php');
 echo '<br>' . hash_file('sha512', 'pago.php');
 echo  '<br><br>';
 //echo '<br>' . hash_file('md5', 'F:\MATERIAL.PROYECTO\EVIDENCIAS\Disk.Partition.Raw.Image');
-echo '<br> Obtener el valor hash de una imagen iso detallando unicamente el tipo de hash, su ruta y nombre: <br>' . hash_file('md5', 'E:\Users\Diego\Desktop\UMG\forense\practica2\DiskPartitionRawImage.dd');
+echo '<br> Obtener el valor hash de una imagen iso detallando unicamente el tipo de hash, su ruta y nombre: <br>' . hash_file('md5', 'E:\Users\Diego\Desktop\UMG\forense\proyectofinal\EmailHeaders.docx');
