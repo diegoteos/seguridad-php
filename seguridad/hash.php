180dc6c0137faf90cc40199c5717b572d1bdb3ea
diegoteos <diegoteos@hotmail.com>
2022-08-26 17:49:40 -0600

    validacion en formularios

--- seguridad/hash.php
+++ seguridad/hash.php
@@ -6,3 +6,4 @@ echo '<br>' . hash_file('sha512', 'pago.php');
 echo  '<br><br>';
 //echo '<br>' . hash_file('md5', 'F:\MATERIAL.PROYECTO\EVIDENCIAS\Disk.Partition.Raw.Image');
 echo '<br> Obtener el valor hash de una imagen iso detallando unicamente el tipo de hash, su ruta y nombre: <br>' . hash_file('md5', 'E:\Users\Diego\Desktop\UMG\forense\proyectofinal\EmailHeaders.docx');
+
---
+++ seguridad/validaciones/frontend.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validacion de formularios</title>
+</head>
+<body>
+    <h1>Validaciones del lado del cliente:</h1>
+    <li>Forzar que un campo sea lleno y no pueda habilitarse la opcion enviar hasta que todos los campos esten llenos, ejemplo:</li>
+    <p></p>
+</body>
+</html>
