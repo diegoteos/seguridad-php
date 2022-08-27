90f7e61800d87b378b78e7482ba6f418bf1e0d9a
diegoteos <diegoteos@hotmail.com>
2022-08-26 18:10:43 -0600

    encabezado

---
+++ seguridad/validaciones/encabezado.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+
+</head>
+
+<body>
+    <div class="container">
+        <hr class="my-2">
+        <nav class=" navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+
+                </div>
+            </div>
+        </nav>
+        <nav class=" navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="navbar-brand" href="frontend.php">Seguridad fronend</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <hr class="my-2">
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
--- seguridad/validaciones/frontend.php
+++ seguridad/validaciones/frontend.php
@@ -1,14 +1,45 @@
+<?php
+include_once 'encabezado.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Validacion de formularios</title>
 </head>
+
 <body>
-    <h1>Validaciones del lado del cliente:</h1>
-    <li>Forzar que un campo sea lleno y no pueda habilitarse la opcion enviar hasta que todos los campos esten llenos, ejemplo:</li>
-    <p></p>
+    <div class="container">
+        <br>
+        <h1>Validaciones del lado del cliente:</h1>
+        <hr>
+        <div class="container">
+            <div class="container">
+                <li>Forzar que un campo sea lleno y no pueda habilitarse la opcion enviar hasta que todos los campos esten llenos, ejemplo:</li>
+                <p>Para ello tenemos el atributo required es usado para indicar al usuario que un input es requerido en un elemento antes de que un form pueda ser enviado.</p>
+            </div>
+        </div>
+        <form action="frontend.php" method="post">
+            <strong>Campo obligatorio: </strong><input type="text" name="primercampo" id="primercampo" required="hola">
+            <button type="submit">Enviar</button>
+        </form>
+        <hr>
+        <div class="container">
+            <div class="container">
+                <li>Declarar el tipo de datos que se espera, si es texto, numero, correo... ejemmplo:</li>
+                <p>Para ello definimos los input como txt, email, password, number, otros.</p>
+            </div>
+        </div>
+        <form action="frontend.php" method="post">
+        <strong>Tipo correo electronico:</strong>    
+        <input type="email" name="primercampo" id="primercampo" required="hola">
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
 </body>
+
 </html>
