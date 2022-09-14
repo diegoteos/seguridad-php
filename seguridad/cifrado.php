91cc613ca46b444c473421c507fd34d759c23359
diegoteos <diegoteos@hotmail.com>
2022-09-13 20:36:21 -0600

    cifrado de contraseñas con password_hash

--- seguridad/cifrado.php
+++ seguridad/cifrado.php
@@ -1,13 +1,27 @@
 <?php
-$pass = 'hola';
+$pass = 'holaj';
 
 echo '<br>';
 $password = password_hash($pass, PASSWORD_DEFAULT, ['cost' => 5]);
+$p = password_hash($pass, PASSWORD_DEFAULT, ['cost' => 5]);
+echo '<br>';
+echo $password;
+echo '<br>';
+echo $p;
 echo '<br>';
 
-echo '<br>';
-if (password_verify('hola', $password)){  // veerificamos si la contraseña ingresada es igual a la segunda que debe estar en la base de datos
-echo "las contraseñas son idugales";
+
+if (password_verify($pass, $password)){  // veerificamos si la contraseña ingresada es igual a la segunda que debe estar en la base de datos
+    echo "las contraseñas son igugales";
+    echo '<br>';
 } else  {
     echo "las contraseñas no son idugales";
+    echo '<br>';
+}
+if (password_verify('holaj', $p)){  // veerificamos si la contraseña ingresada es igual a la segunda que debe estar en la base de datos
+    echo "las contraseñas son igugales";
+    echo '<br>';
+} else  {
+    echo "las contraseñas no son idugales";
+    echo '<br>';
 }
