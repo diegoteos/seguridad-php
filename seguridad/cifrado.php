becac28db7e0b6831cc6dd60bd0faed997537ff9
diegoteos <diegoteos@hotmail.com>
2022-09-16 22:51:37 -0600

    pass

--- seguridad/cifrado.php
+++ seguridad/cifrado.php
@@ -1,4 +1,25 @@
 <?php
+$cifrar = 'usuario';
+
+echo '<br>';
+$contraseña_cifrada = password_hash($cifrar, PASSWORD_DEFAULT, ['cost' => 10]);
+echo '<br>';
+echo $contraseña_cifrada;
+echo '<br>';
+
+
+if (password_verify($cifrar, $contraseña_cifrada)){  // veerificamos si la contraseña ingresada es igual a la segunda que debe estar en la base de datos
+    echo "las contraseñas son igugales";
+    echo '<br>';
+} else  {
+    echo "las contraseñas no son idugales";
+    echo '<br>';
+}
+
+
+/*
+
+<?php
 
 // validamos que vengan los dos parametros con los nombres que les corresponden 
 if (isset($_POST['usuario']) && isset($_POST['contraseña'])) {
